6a5742839a24bbc015b6a3930cc0d457c971435e
TylerCurtis011 <145739097+TylerCurtis011@users.noreply.github.com>
2024-05-01 15:36:02 -0500

    testLSFR

--- LFSR_STUFF/LFSR.sv
+++ LFSR_STUFF/LFSR.sv
@@ -20,7 +20,7 @@ module LFSR #(parameter Width = 4) (
     //     endcase
 
     //mux2 #(4) m1({data_out[3]^data_out[0], data_out[3:1]}, seed, select, m2f);
-    
+
     if(Width == 3) begin
         assign lead_bit = data_out[2]^data_out[0];
     end
@@ -34,7 +34,7 @@ module LFSR #(parameter Width = 4) (
 
     //00110110
     else if(Width == 6) begin
-        assign lead_bit = data_out[5]^data_out[4]^data_out[2]^data_out[1];
+        assign lead_bit = data_out[6]^data_out[5];
     end
 
     //01101001
@@ -44,13 +44,44 @@ module LFSR #(parameter Width = 4) (
 
    // 10100110
     else if(Width == 8) begin
-        assign lead_bit = data_out[7]^data_out[5]^data_out[2]^data_out[1];
+        assign lead_bit = data_out[8]^data_out[6]^data_out[5]^data_out[4];
     end
 
    // 000101111100
     else if(Width == 9) begin
-        assign lead_bit = data_out[8]^data_out[6]^data_out[5]^data_out[4]^data_out[3]^data_out[2];
+        assign lead_bit = data_out[9]^data_out[5];
     end
+    
+//     if(Width == 3) begin
+//         assign lead_bit = data_out[2]^data_out[0];
+//     end
+//     else if(Width == 4) begin
+//         assign lead_bit = data_out[3]^data_out[0];
+//     end
+//     //00011101
+//     else if(Width == 5) begin
+//         assign lead_bit = data_out[4]^data_out[3]^data_out[2]^data_out[0];
+//     end
+
+//     //00110110
+//     else if(Width == 6) begin
+//         assign lead_bit = data_out[5]^data_out[4]^data_out[2]^data_out[1];
+//     end
+
+//     //01101001
+//     else if(Width == 7) begin
+//         assign lead_bit = data_out[6]^data_out[5]^data_out[3]^data_out[0];
+//     end
+
+//    // 10100110
+//     else if(Width == 8) begin
+//         assign lead_bit = data_out[7]^data_out[5]^data_out[2]^data_out[1];
+//     end
+
+//    // 000101111100
+//     else if(Width == 9) begin
+//         assign lead_bit = data_out[8]^data_out[6]^data_out[5]^data_out[4]^data_out[3]^data_out[2];
+//     end
     
     flopenl #(Width) f1(clk, load, enable, {lead_bit, data_out[Width-1:1]}, seed, data_out);
 
